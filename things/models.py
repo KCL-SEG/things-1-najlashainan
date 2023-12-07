46cbb32ab41e4fc068c9e930ea313c0aa2a9108d
Najla Shainan <najlanassersh@gmail.com>
2023-12-08 00:23:53 +0300

    fix error in excercise 2

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Things(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=512)
     quantity = models.IntegerField()
